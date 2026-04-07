050c063a9d8d693c8c8bb8af482b0c41f578c5ce
ronnie <ronnienyamhute81@gmail.com>
2026-04-07 15:16:28 +0200

    "Bug fixes and mobile menu fix "

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, OnDestroy, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -9,39 +9,60 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
-export class Header { // Renamed from HeaderComponent to Header to fix app.ts error
+export class Header implements OnDestroy {
   isScrolled = false;
   isMobileMenuOpen = false;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2
   ) {}
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.isScrolled = window.scrollY > 20;
     }
   }
 
-  toggleMobileMenu() {
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    if (isPlatformBrowser(this.platformId) && window.innerWidth > 992 && this.isMobileMenuOpen) {
+      this.closeMobileMenu();
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscapeKey(): void {
+    if (this.isMobileMenuOpen) {
+      this.closeMobileMenu();
+    }
+  }
+
+  toggleMobileMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
     this.updateBodyScroll();
   }
 
-  closeMobileMenu() {
+  closeMobileMenu(): void {
     this.isMobileMenuOpen = false;
     this.updateBodyScroll();
   }
 
-  private updateBodyScroll() {
+  private updateBodyScroll(): void {
     if (isPlatformBrowser(this.platformId)) {
       if (this.isMobileMenuOpen) {
-        this.renderer.addClass(document.body, 'no-scroll');
+        this.renderer.addClass(this.document.body, 'no-scroll');
       } else {
-        this.renderer.removeClass(document.body, 'no-scroll');
+        this.renderer.removeClass(this.document.body, 'no-scroll');
       }
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.removeClass(this.document.body, 'no-scroll');
     }
   }
 }
